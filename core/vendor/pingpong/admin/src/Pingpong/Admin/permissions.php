51ef13cb399819f1785a883493d2bbe7c60d22cd
emelyanovtv <timofey@MacBook-Air.local>
2015-06-02 11:14:49 +0300

    new filters added

--- core/vendor/pingpong/admin/src/Pingpong/Admin/permissions.php
+++ core/vendor/pingpong/admin/src/Pingpong/Admin/permissions.php
@@ -12,11 +12,19 @@ if(Auth::check())
 	{
 		
 	}
-
 	Trusty::when(['admin/users', 'admin/users/*'], 'manage_users');
 	Trusty::when(['admin/pages', 'admin/pages/*'], 'manage_pages');
 	Trusty::when(['admin/articles', 'admin/articles/*'], 'manage_articles');
 	Trusty::when(['admin/categories', 'admin/categories/*'], 'manage_categories');
+    Trusty::when(['admin/operations', 'admin/operations/*'], 'manage_operations');
+    Trusty::when(['admin/storage', 'admin/storage/*'], 'manage_storage');
+    Trusty::when(['admin/events', 'admin/events/*'], 'manage_events');
+    Trusty::when('admin/eventprops', 'manage_eventprops');
+    Trusty::when('admin/product', 'manage_product');
+    Trusty::when('admin/productgroups', 'manage_productgroups');
+    Trusty::when('admin/measures', 'manage_measures');
+    Trusty::when('admin/measures', 'manage_measures');
+    Trusty::when('admin/properties', 'manage_properties');
 	Trusty::when(['admin/permissions', 'admin/permissions/*'], 'manage_permissions');
 	Trusty::when(['admin/roles', 'admin/roles/*'], 'manage_roles');
 	Trusty::when('admin/settings', 'manage_settings');
