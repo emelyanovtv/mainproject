e15600830a282bba91e1b6e355f41d7166f6a6fa
emelyanovtv <timofey@MacBook-Air.local>
2015-07-22 16:29:58 +0300

    app config

--- core/app/controllers/HomeController.php
+++ core/app/controllers/HomeController.php
@@ -1,23 +1,39 @@
 <?php
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
+use Pingpong\Admin\Entities\StorageEvents;
+use Pingpong\Admin\Entities\StorageEventsMaterials;
+use Pingpong\Admin\Entities\StorageHasEventsMaterials;
+use Pingpong\Admin\Entities\StorageHasMaterial;
+use Pingpong\Admin\Entities\Storages;
 
 class HomeController extends BaseController {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
 
 	public function showWelcome()
 	{
-		return View::make('hello');
+        $list = Storages::with(
+                'hasMaterials.materials.materialsgroup'
+
+        )->get();
+        $materialConfig = Config::get('app.materialsConfig');
+        $dataArr = [];
+        foreach($list as $storage)
+        {
+            $dataArr[$storage->name] = [];
+            if(count($storage->hasMaterials))
+            {
+                foreach($storage->hasMaterials as $item)
+                {
+                    if(in_array($item->materials->material_group_id,$materialConfig['group_ids']) && array_key_exists($item->materials->id, $materialConfig['materials']))
+                    {
+                        $dataArr[$storage->name][] = $item;
+                    }
+
+                }
+            }
+        }
+        return View::make('pages.hello', compact('dataArr'));
 	}
 
 }
